c5860645d6b6d5569374559905e250424a588415
lffw8888 <72547811+lffw8888@users.noreply.github.com>
2026-07-21 18:45:02 +0800

    Update custom_feed.sh
    Signed-off-by: lffw8888 <72547811+lffw8888@users.noreply.github.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -165,9 +165,8 @@ install_custom_feed() {
         lucky luci-app-lucky luci-app-openclash luci-app-homeproxy luci-app-amlogic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
         msd_lite luci-app-msd_lite cups luci-app-cupsd
-        tailscale luci-app-tailscale
+        tailscale luci-app-tailscale \
         luci-app-advanced luci-app-ap-modem luci-app-wan-mac
-
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
